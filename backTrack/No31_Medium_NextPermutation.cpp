1f2c24be86e7a48ab1ae9b947ab43380b03e2108
czqInNanjing <czqofnju@gmail.com>
2017-08-18 13:10:03 +0800

    some problems

---
+++ backTrack/No31_Medium_NextPermutation.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Qiang Chen on 8/18/17.
+//
+//https://leetcode.com/problems/next-permutation/description/
+//Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+//
+//If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+//
+//The replacement must be in-place, do not allocate extra memory.
+//
+//Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+//1,2,3 → 1,3,2
+//3,2,1 → 1,2,3
+//1,1,5 → 1,5,1
+//
+// 不能算回溯的题目，但是可以用在解回溯的题目 No46,47 从开始一个一个寻找下一个permutation直到回到最开始就可以了，时间效率也还可以吧
+//
+#include <vector>
+using namespace std;
+class No31_Medium_NextPermutation{
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size() <= 1)
+            return;
+
+        int index = -1;
+        for(int i = nums.size() - 2 ; i >=0 ; i-- ) {
+            if(nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1 ) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        int nextBig = -1;
+        for(int i = nums.size() - 1; i > index; i-- ) {
+            if(nums[i] > nums[index]){
+                nextBig = i;
+                break;
+            }
+        }
+        swap(nums[index], nums[nextBig]);
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+};
--- backTrack/No47_Medium_Permutation2.cpp
+++ backTrack/No47_Medium_Permutation2.cpp
@@ -15,46 +15,139 @@
 using namespace std;
 
 class No47_Medium_Permutation2 {
+
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    static vector<vector<int>> permuteUnique(vector<int>& nums) {
+        if(nums.empty())
+            return vector<vector<int>>();
+
         vector<vector<int>> result;
         vector<int> tempResult;
-        permute(result, nums, 0);
+        sort(nums.begin(), nums.end());
+        bool used[nums.size()] = {false};
+        permute(result, nums, tempResult, used);
 
         return result;
     }
 private:
-    void permute(vector<vector<int>> &result, vector<int>& nums, int begin) {
+    static void permute(vector<vector<int>> &result, vector<int>& nums, vector<int>& tempResult, bool *used) {
 
-        if( begin >= nums.size() ) {
-            result.push_back(nums);
+        if(tempResult.size() == nums.size()){
+            result.push_back(tempResult);
             return;
         }
+        reverse(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            if(used[i]) continue;
 
+            if(i>0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
 
-        permute(result, nums, begin + 1);
-        for( int i = begin + 1 ; i < nums.size() ; ++i ) {
-            set<int> existNums;
-            if(nums[begin] != nums[i] ) {
-                if(existNums.find(nums[i]) == existNums.end()) {
-                    mySwap(nums, begin, i);
-                    permute(result, nums, begin + 1);
-                    mySwap(nums, begin, i);
-                    existNums.insert(nums[i]);
-                }
+            used[i] = true;
+            tempResult.push_back(nums[i]);
+            permute(result, nums, tempResult, used);
+            used[i] = false;
+            tempResult.pop_back();
 
-            }
 
         }
 
 
 
     }
-    void mySwap(vector<int> &nums, int x, int y) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
-    }
+// 解法2 利用next permutation
+//public:
+//    vector<vector<int>> permuteUnique(vector<int>& nums) {
+//        sort(nums.begin(), nums.end());
+//        vector<vector<int>> result;
+//        vector<int> tempResult(nums);
+//        result.push_back(tempResult);
+//        int size = nums.size();
+//        while(true) {
+//            nextPermutation(tempResult);
+//            bool theSame = true;
+//            for(int i = 0 ; i < size ; ++i) {
+//                if(tempResult[i] != nums[i]) {
+//                    theSame = false;
+//                    break;
+//                }
+//            }
+//            if(theSame)
+//                return result;
+//            else
+//                result.push_back(tempResult);
+//        }
+//
+//
+//
+//
+//    }
+//private:
+//    void nextPermutation(vector<int>& nums) {
+//        if(nums.size() <= 1)
+//            return;
+//
+//        int index = -1;
+//        for(int i = nums.size() - 2 ; i >=0 ; i-- ) {
+//            if(nums[i] < nums[i + 1]) {
+//                index = i;
+//                break;
+//            }
+//        }
+//
+//        if(index == -1 ) {
+//            reverse(nums.begin(), nums.end());
+//            return;
+//        }
+//        int nextBig = -1;
+//        for(int i = nums.size() - 1; i > index; i-- ) {
+//            if(nums[i] > nums[index]){
+//                nextBig = i;
+//                break;
+//            }
+//        }
+//        swap(nums[index], nums[nextBig]);
+//        reverse(nums.begin() + index + 1, nums.end());
+//    }
+//
+
+
+
+// 可行解法3，但是私有方法里的nums必须不能使用引用，会破坏循环
+//public:
+//    vector<vector<int>> permuteUnique(vector<int>& nums) {
+//        vector<vector<int>> result;
+//        sort(nums.begin(), nums.end());
+//        permute(result, nums, 0);
+//
+//        return result;
+//    }
+//private:
+//    void permute(vector<vector<int>> &result, vector<int> nums, int begin) {
+//
+//        if( begin == nums.size() ) {
+//            result.push_back(nums);
+//            return;
+//        }
+//
+//
+//        for( int i = begin ; i < nums.size() ; ++i ) {
+//            if(i != begin && nums[begin] == nums[i] ) continue;
+//
+//            mySwap(nums, begin, i);
+//            permute(result, nums, begin + 1);
+//
+//
+//        }
+//
+//
+//
+//    }
+//    void mySwap(vector<int> &nums, int x, int y) {
+//        int temp = nums[x];
+//        nums[x] = nums[y];
+//        nums[y] = temp;
+//    }
+
 
 
 };
